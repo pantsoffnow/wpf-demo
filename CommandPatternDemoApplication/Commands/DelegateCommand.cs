3c4b8be23714bcb338511a3122e9223b1af3c4b7
Christopher Gordon <christophergordon80@gmail.com>
2018-07-01 21:20:41 -0500

    Initial commit

---
+++ CommandPatternDemoApplication/Commands/DelegateCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CommandPatternDemoApplication.Commands
+{
+    public class DelegateCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        private readonly Action _action;
+        private readonly Func<bool> _canExecute;
+
+        public DelegateCommand(Action action, Func<bool> canExecute = null)
+        {
+           _action = action;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute != null)
+            {
+                return _canExecute.Invoke();
+            }
+
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action.Invoke();
+        }
+    }
+}
---
+++ CommandPatternDemoApplication/ViewModels/NameViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
+using CommandPatternDemoApplication.Commands;
+
+namespace CommandPatternDemoApplication.ViewModels
+{
+    public class NameViewModel : INotifyPropertyChanged
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name == value)
+                    return;
+                _name = value;
+
+                OnPropertyChanged();
+                ((DelegateCommand)SubmitCommand).CanExecute(null);
+            }
+        }
+
+        public ICommand SubmitCommand { get; private set; }
+
+        public NameViewModel()
+        {
+            SubmitCommand = new DelegateCommand(ProcessSubmitCommand, ProcessSubmitCanExecute);
+        }
+
+        private bool ProcessSubmitCanExecute()
+        {
+            return !string.IsNullOrEmpty(Name);
+        }
+
+        private void ProcessSubmitCommand()
+        {
+            //Persist Data
+            Console.WriteLine(@"Processing Submit Command.");
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        } 
+        #endregion
+    }
+}
